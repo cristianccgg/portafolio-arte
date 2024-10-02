cb55c86aa293c579660598837856bed041b671d5
cristianccgg <cristianccggg@gmail.com>
2024-10-02 17:11:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Cambia 'repo-name' por el nombre de tu repositorio
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/portfolio-arte/", // Asegúrate de poner el nombre correcto de tu repositorio
+});
